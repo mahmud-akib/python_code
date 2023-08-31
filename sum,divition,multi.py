aa1464e95d1758889dabde7c4452fbcaa67ed614
Akib Mahmud <101253688+mahmud-akib@users.noreply.github.com>
2023-09-01 00:57:48 +0600

    Add files via upload

---
+++ sum,divition,multi.py
@@ -0,0 +1,14 @@
+num1 = input("Enter the num1 : ")
+num2 = input("Enter the num2 : ")
+
+sum = int(num1) + int(num2)
+print("The sum of these numbers is : " + str(sum))
+
+subtraction = int(num1) - int(num2)
+print("The subtraction of these numbers is : " + str(subtraction))
+
+multi = int(num1) * int(num2)
+print("The multiplication of these numbers is : " + str(multi))
+
+divition = int(num1) / int(num2)
+print("The divition of these numbers is : " + str(divition))
